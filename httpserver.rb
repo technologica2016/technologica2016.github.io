d63f6c1826ca211ab36f9803b8626940682dc4c2
Tomoya Tabuchi <t@tomoyat1.com>
2016-05-22 12:41:45 +0900

    minor fix to server script

--- httpserver.rb
+++ httpserver.rb
@@ -3,10 +3,20 @@
 require 'webrick'
 include WEBrick
 
-s = HTTPServer.new(
-  :Port => 8000,
-  :DocumentRoot => File.join(Dir::pwd),
-  :BindAddress => '0.0.0.0'
-)
+if ARGV.length < 1 then
+  puts 'binding to localhost'
+  s = HTTPServer.new(
+    :Port => 8000,
+    :DocumentRoot => File.join(Dir::pwd)
+  )
+else
+  puts "binding to #{ARGV[0]}"
+  s = HTTPServer.new(
+    :Port => 8000,
+    :DocumentRoot => File.join(Dir::pwd),
+    :BindAddress => ARGV[0]
+  )
+end
+
 trap("INT"){ s.shutdown }
 s.start
